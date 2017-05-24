46e4e5106dbc1834938e378f5c5a53648c0f52b7
MarioK <mario.kiefer@a1.net>
2017-05-24 02:20:12 +0200

    more comments and lets

--- main.js
+++ main.js
@@ -1,18 +1,101 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var CELL_STATE;
+(function (CELL_STATE) {
+    CELL_STATE[CELL_STATE["REVEALED"] = 0] = "REVEALED";
+    CELL_STATE[CELL_STATE["MARKED"] = 1] = "MARKED";
+    CELL_STATE[CELL_STATE["NOT_MARKED"] = 2] = "NOT_MARKED";
+    CELL_STATE[CELL_STATE["HIDDEN"] = 3] = "HIDDEN";
+})(CELL_STATE || (CELL_STATE = {}));
+var GameCell = (function (_super) {
+    __extends(GameCell, _super);
+    function GameCell() {
+        var _this = _super.call(this) || this;
+        _this.style.backgroundColor = "dimgray";
+        return _this;
+    }
+    Object.defineProperty(GameCell.prototype, "value", {
+        set: function (v) {
+            this._value = v;
+            this.innerHTML = String(v);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     * toogleMarked
+     */
+    GameCell.prototype.toogleMarked = function () {
+        if (this._state === CELL_STATE.NOT_MARKED) {
+            this.mark();
+        }
+        else {
+            this.unmark();
+        }
+    };
+    /**
+     * setMarked
+     */
+    GameCell.prototype.mark = function () {
+        this.style.backgroundColor = "yellow";
+        this._state = CELL_STATE.MARKED;
+    };
+    /**
+     * setUnmarked
+     */
+    GameCell.prototype.unmark = function () {
+        this.style.backgroundColor = "dimgray";
+        this._state = CELL_STATE.MARKED;
+    };
+    /**
+     * reveal
+     */
+    GameCell.prototype.reveal = function () {
+        this.style.fontSize = "48px";
+        this.style.backgroundColor = "dimgray";
+        this._state = CELL_STATE.REVEALED;
+    };
+    return GameCell;
+}(HTMLTableCellElement));
+var InfoCell = (function (_super) {
+    __extends(InfoCell, _super);
+    function InfoCell(parameters) {
+        return _super.call(this) || this;
+    }
+    /**
+     * setInfo
+     */
+    InfoCell.prototype.setInfo = function (numBombs, sumPoints) {
+        this._numBombs = numBombs;
+        this._sumPoints = sumPoints;
+        this.innerHTML = "B: " + this._numBombs + "<br>P: " + this._sumPoints;
+    };
+    return InfoCell;
+}(HTMLTableCellElement));
 document.addEventListener("DOMContentLoaded", function (event) {
     var gameField = document.getElementById("gameField");
+    var numRows = gameField.rows.length;
+    var playField = gameField.cloneNode(true);
+    var infoBoxesRows = [].slice.call(playField.rows);
     var scoreField = document.getElementById("scoreField");
-    var rows = gameField.rows.length;
+    playField.deleteRow(numRows - 1);
     var level = 2;
     var sumGame = 1;
     var sumColVals = [0, 0, 0, 0, 0];
     var numCol0s = [0, 0, 0, 0, 0];
     var numRow0123 = [0, 0, 0, 0];
+    var cols;
+    // disable "normal" right click on gameField 
     gameField.oncontextmenu = function () {
         return false;
     };
-    for (var i = 0; i < rows - 1; i += 1) {
+    // initialise gameField
+    for (var i = 0; i < numRows - 1; i += 1) {
         var row = gameField.rows[i];
-        var cols = row.cells.length;
+        cols = row.cells.length;
         var sumRowVals = 0;
         numRow0123 = [0, 0, 0, 0];
         for (var j = 0; j < cols - 1; j += 1) {
@@ -32,6 +115,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             if (rand == 3) {
                 rand = (sumRow0123[3] >= 1 + level) ? 1 : rand;
             }*/
+            // write random value to current cell
             cell.innerHTML = String(rand);
             numRow0123[rand]++;
             if (rand === 0) {
@@ -39,7 +123,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
             sumRowVals += rand;
             sumColVals[j] += rand;
+            // "hide" cell value
             cell.style.fontSize = "0px";
+            // define right click behaviour
             cell.oncontextmenu = function () {
                 if (this.style.backgroundColor !== "yellow" && this.style.fontSize == "0px") {
                     this.style.backgroundColor = "yellow";
@@ -48,6 +134,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     this.style.backgroundColor = "dimgray";
                 }
             };
+            //define left click behaviour
             cell.onclick = function () {
                 if (this.style.fontSize == "0px") {
                     this.style.fontSize = "48px";
@@ -60,15 +147,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 }
             };
         }
+        // write info cell for current row
         row.cells[cols - 1].innerHTML = "B: " + numRow0123[0] + "<br>P: " + sumRowVals;
         row.cells[cols - 1].style.fontSize = "32px";
     }
-    var inforow = gameField.rows[rows - 1];
+    // write info cells for columns
+    var inforow = gameField.rows[numRows - 1];
     for (var i = 0; i < cols - 1; i++) {
         inforow.cells[i].innerHTML = "B: " + numCol0s[i] + "<br>P: " + sumColVals[i];
         inforow.cells[i].style.fontSize = "32px";
     }
+    function checkFinished() {
+        var finished = true;
+        return true;
+    }
 });
-function checkFinished() {
-    return true;
-}
--- main.ts
+++ main.ts
@@ -1,25 +1,110 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var gameField: HTMLTableElement = <HTMLTableElement> document.getElementById("gameField");
-    var scoreField = document.getElementById("scoreField");
-    var rows = gameField.rows.length;
-    var level = 2;
-    var sumGame = 1;
+enum CELL_STATE {
+    REVEALED,
+    MARKED,
+    NOT_MARKED,
+    HIDDEN
+}
+class GameCell extends HTMLTableCellElement {
+    private _value: number;
+    private _state: CELL_STATE;
 
-    var sumColVals = [0, 0, 0, 0, 0];
-    var numCol0s = [0, 0, 0, 0, 0];
-    var numRow0123 = [0, 0, 0, 0];
+    constructor() {
+        super();
+        this.style.backgroundColor = "dimgray";
+    }
+
+    public set value(v: number) {
+        this._value = v;
+        this.innerHTML = String(v);
+    }
+
+    /**
+     * toogleMarked
+     */
+    public toogleMarked() {
+        if (this._state === CELL_STATE.NOT_MARKED) {
+            this.mark();
+        } else {
+            this.unmark();
+        }
+    }
+
+    /**
+     * setMarked
+     */
+    private mark() {
+        this.style.backgroundColor = "yellow";
+        this._state = CELL_STATE.MARKED;
+    }
+
+    /**
+     * setUnmarked
+     */
+    private unmark() {
+        this.style.backgroundColor = "dimgray";
+        this._state = CELL_STATE.MARKED;
+    }
+
+    /**
+     * reveal
+     */
+    public reveal() {
+        this.style.fontSize = "48px";
+        this.style.backgroundColor = "dimgray";
+        this._state = CELL_STATE.REVEALED;
+    }
+}
+
+
+class InfoCell extends HTMLTableCellElement {
+    private _numBombs: number;
+    private _sumPoints: number;
+
+    constructor(parameters) {
+        super();
+    }
+
+    /**
+     * setInfo
+     */
+    public setInfo(numBombs: number, sumPoints: number) {
+        this._numBombs = numBombs;
+        this._sumPoints = sumPoints;
+        this.innerHTML = "B: " + this._numBombs + "<br>P: " + this._sumPoints;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let gameField: HTMLTableElement = <HTMLTableElement>document.getElementById("gameField");
+    let numRows: number = gameField.rows.length;
+    let playField: HTMLTableElement = <HTMLTableElement>gameField.cloneNode(true);
+    let infoBoxesRows: HTMLTableRowElement[];
+
+    let scoreField: HTMLSpanElement = document.getElementById("scoreField");
+    playField.deleteRow(numRows-1);
+    let level: number = 2;
+
+    let sumGame: number = 1;
+    let sumColVals: number[] = [0, 0, 0, 0, 0];
+    let numCol0s: number[] = [0, 0, 0, 0, 0];
+    let numRow0123: number[] = [0, 0, 0, 0];
+    let cols: number;
+
+    // disable "normal" right click on gameField 
     gameField.oncontextmenu = function () {
         return false;
     }
-    for (var i = 0; i < rows - 1; i += 1) {
-        var row = gameField.rows[i];
-        var cols = row.cells.length;
-        var sumRowVals = 0;
+
+    // initialise gameField
+    for (let i = 0; i < numRows - 1; i += 1) {
+        let row: HTMLTableRowElement = gameField.rows[i];
+        cols = row.cells.length;
+        let sumRowVals: number = 0;
         numRow0123 = [0, 0, 0, 0];
-        for (var j = 0; j < cols - 1; j += 1) {
-            var cell = row.cells[j];
+        for (let j = 0; j < cols - 1; j += 1) {
+            let cell: GameCell = <GameCell>row.cells[j];
             //random int between 0 and int (excl.)
-            var rand = Math.floor(Math.random() * 4);
+            let rand: number = Math.floor(Math.random() * 4);
             /*
             if (rand == 0) {
                 rand = (sumRow0123[0] >= 5 + level) ? 1 : rand;
@@ -34,6 +119,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 rand = (sumRow0123[3] >= 1 + level) ? 1 : rand;
             }*/
 
+            // write random value to current cell
             cell.innerHTML = String(rand);
             numRow0123[rand]++;
             if (rand === 0) {
@@ -41,7 +127,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
             sumRowVals += rand;
             sumColVals[j] += rand;
+
+            // "hide" cell value
             cell.style.fontSize = "0px";
+
+            // define right click behaviour
             cell.oncontextmenu = function () {
                 if (this.style.backgroundColor !== "yellow" && this.style.fontSize == "0px") {
                     this.style.backgroundColor = "yellow";
@@ -49,6 +139,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     this.style.backgroundColor = "dimgray";
                 }
             }
+
+            //define left click behaviour
             cell.onclick = function () {
                 if (this.style.fontSize == "0px") {
                     this.style.fontSize = "48px";
@@ -62,18 +154,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
         }
 
+        // write info cell for current row
         row.cells[cols - 1].innerHTML = "B: " + numRow0123[0] + "<br>P: " + sumRowVals;
         row.cells[cols - 1].style.fontSize = "32px";
     }
-    var inforow = gameField.rows[rows - 1]
-    for (var i = 0; i < cols - 1; i++) {
+
+    // write info cells for columns
+    let inforow = gameField.rows[numRows - 1]
+    for (let i = 0; i < cols - 1; i++) {
         inforow.cells[i].innerHTML = "B: " + numCol0s[i] + "<br>P: " + sumColVals[i];
         inforow.cells[i].style.fontSize = "32px";
     }
 
-
+    function checkFinished(): boolean {
+        let finished: boolean = true;
+        return true;
+    }
 });
 
-function checkFinished(): boolean {
-    return true;
-}
