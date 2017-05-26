3e575afb570256239268936175003232be1dfa29
MarioK <mario.kiefer@a1.net>
2017-05-26 14:56:52 +0200

    implemented levels

--- classes/VoltorbFlip.ts
+++ classes/VoltorbFlip.ts
@@ -5,15 +5,33 @@ export class VoltorbFlip {
     private level: number;
     private scoreSpan: HTMLSpanElement;
     constructor(table: HTMLTableElement) {
-        this.gameTable = new GameTable(table);
-        this.init();
         this.score = 1;
         this.level = 1;
+        this.init();
         this.scoreSpan = document.getElementById("scoreField");
     }
+    private generateTable = (): HTMLTableElement => {
+        let oldTable = document.getElementById("gameField");
+        if (oldTable !== null) {
+            document.body.removeChild(oldTable);
+        }
 
+        let dynTable = document.createElement("table");
+        dynTable.id = "gameField";
+        dynTable.className = "game inline";
+        for (var i = 0; i < this.level + 5; i++) {
+            var tr: HTMLTableRowElement = document.createElement('tr');
+            for (var j = 0; j < this.level + 5; j++) {
+                var td: HTMLTableCellElement = document.createElement("td");
+                tr.appendChild(td);
+            }
+            dynTable.appendChild(tr);
+        }
+        document.body.appendChild(dynTable);
+        return dynTable;
+    }
     private init = () => {
-
+        this.gameTable = new GameTable(this.generateTable());
         let rand: number = 0;
         this.gameTable.rows.forEach((row, i) => {
             let max0s: number = this.level;
@@ -24,7 +42,7 @@ export class VoltorbFlip {
                 if (curr0 >= max0s) {
                     rand = Math.floor(Math.random() * 3) + 1;
                 } else if (curr0 <= min0s) {
-                    
+                    rand = Math.floor(Math.random() * 4);
                 } else {
                     rand = Math.floor(Math.random() * 4);
                 }
@@ -37,12 +55,18 @@ export class VoltorbFlip {
                 cell._cell.onclick = () => {
                     var cellValue = cell.reveal();
                     if (cellValue === 0) {
-                        alert("Game Over. Score: " + this.score)
+                        alert("Game Over. Score: " + this.score);
+                        this.score = 1;
+                        this.level = 1;
+                        this.scoreSpan.innerHTML = String(this.score);
+                        this.init();
                     } else if (cellValue !== -1) {
                         this.score *= cellValue;
                         this.scoreSpan.innerHTML = String(this.score);
                         if (this.checkFinished()) {
                             alert("Congratulations! Score: " + this.score);
+                            this.level++;
+                            this.init();
                         }
                     }
                 }
